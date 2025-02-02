596f5a12f33268df8d5c5cd5f7d1ae2e3a23d465
DanielConCode <esau02con@gmail.com>
2025-02-02 11:39:11 -0600

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function agregarAmigo(){
     //Validar la entrada, si está vacío, mostrar un alert con un mensaje de error
     if(nombre == "" ){
         alert("Por favor, inserte un nombre");
-        agregarAmigo();
+        //-agregarAmigo()-; Corrigiendo, Este codigo es inncesario ya que no es necesario iniciar la funcion de nuevo, esto causa un bucle infinito del error
     }
     else {//Actualizar el array de amigos
         amigos.push(nombre);        
@@ -15,4 +15,23 @@ function agregarAmigo(){
     
     //Limpiar el campo de entrada: 
     document.getElementById("amigo").value = "";
+    actualizarLista();
+}
+
+/*Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro 
+de una lista HTML. Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.*/
+
+function actualizarLista(){
+    //Obtener el elemento de la lista
+    let lista = document.querySelector("#listaAmigos");
+
+    //Limpiar la lista existente
+    lista.innerHTML = "";
+
+    //Iterar sobre el arreglo
+    for(var i = 0; i < amigos.length; i++){
+        let elementoLista = document.createElement("li"); //Crea un elemento <li> para la lista <ul> seleccionada en la variable "lista"
+        elementoLista.textContent = amigos[i]; //Se asigna el "textContent" del array amigos correspondiente
+        lista.appendChild(elementoLista); //appendChild: Agrega un elemento como hijo de otro. En este caso agrega un <li> a la lista <ul>
+    }
 }
